d016dae354fd07056bf14b0b16ebec6fc636fd71
ridaarif98 <ridaarif16@gmail.com>
2021-06-30 11:55:58 +0200

    Resolve script errors

--- script.js
+++ script.js
@@ -148,8 +148,7 @@ const allCards = [
 ];
 
 function languages(languages) {
-  return `${languages.map(function test(language) {
- return `<li>${language}</li>`;
+  return `${languages.map((language)=> { `<li>${language}</li>`;
   }).join('')}`;
 }
 
